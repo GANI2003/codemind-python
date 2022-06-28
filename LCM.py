0e1424cbac5ae5b128cc72b5d58b929d3085565c
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-06-28 19:30:18 +0530

    m,n=map(int,input().split()) for i in range(1,n+1):     c=m*i     if(c%n==0):         print(c)         break

--- LCM.py
+++ LCM.py
@@ -1,5 +1,6 @@
-a,b=map(int,input().split())
-for i in range(a,(a*b)+1):
-    if i%a==0 and i%b==0:
+m,n=map(int,input().split())
+for i in range(1,n+1):
+    c=m*i
+    if(c%n==0):
+        print(c)
         break
-print(i)
