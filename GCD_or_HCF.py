8c1717608067d5ac3b1e78c5469f72300589f4e6
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-06-28 23:34:57 +0530

    a,b=map(int,input().strip().split()) if(a<b):     for i in range(1,a+1):         if(a%i==0) and (b%i==0):             f=i     print(f)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,8 +1,6 @@
-def gcd(a,b):
-    hcf=0
+a,b=map(int,input().strip().split())
+if(a<b):
     for i in range(1,a+1):
-        if a%i==0 and b%i==0:
-            hcf=i
-    print(hcf)
-a,b=map(int,input().split())
-gcd(a,b)
+        if(a%i==0) and (b%i==0):
+            f=i
+    print(f)
