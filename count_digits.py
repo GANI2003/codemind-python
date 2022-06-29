ec4bddd5ab6f001ba02e6f4e1185905d20a6d7ca
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-06-29 15:31:08 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) c=0 for i in arr:     if i==0:         c+=1     if i<0:         i=i*-1     while(i):         i=i//10         c+=1     print(c,end=' ')     c=0

---
+++ count_digits.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))
+c=0
+for i in arr:
+    if i==0:
+        c+=1
+    if i<0:
+        i=i*-1
+    while(i):
+        i=i//10
+        c+=1
+    print(c,end=' ')
+    c=0
