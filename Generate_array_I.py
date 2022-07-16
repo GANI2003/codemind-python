c75625467aa2c920789a5f7042651a67d406c78b
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-07-16 19:08:37 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) for i in range(0,len(arr),2):     for j in range(1,arr[i+1]+1):         print(arr[i],end=' ')

--- Generate_array_I.py
+++ Generate_array_I.py
@@ -1,5 +1,5 @@
-n=input()
+n=int(input())
 arr=list(map(int,input().strip().split()))
 for i in range(0,len(arr),2):
     for j in range(1,arr[i+1]+1):
-        print(arr[i],end=" ")
+        print(arr[i],end=' ')
