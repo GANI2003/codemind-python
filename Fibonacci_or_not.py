78e638b7bd7cdf6a0f0308702e8266e0d2bb910b
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-07-16 16:00:40 +0530

    n=int(input()) a,b=0,1 f=0 if(n==1 or n==0):     print(True) for i in range(1,n-1):     c=a+b     if(c==n):         f=1         break     a=b     b=c if(f==1):     print(True) else:     print(False)

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,12 +1,16 @@
 n=int(input())
-n1=0
-n2=1
-for i in range(2,n+1):
-    n3=n1+n2
-    if n3==n:
-        print(True)
+a,b=0,1
+f=0
+if(n==1 or n==0):
+    print(True)
+for i in range(1,n-1):
+    c=a+b
+    if(c==n):
+        f=1
         break
-    n1=n2
-    n2=n3
+    a=b
+    b=c
+if(f==1):
+    print(True)
 else:
     print(False)
