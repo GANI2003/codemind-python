4b6637f0b5fc1c355c1534557ff518fa40602edb
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-10-16 00:06:00 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 for i in arr:     if(i==0 or i==1):         c+=1 if(c==n):     print(True) else:     print(False)

--- Binary_Array.py
+++ Binary_Array.py
@@ -1,10 +1,10 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
+arr=list(map(int,input().split()))
 c=0
-for i in range(n):
-    if arr[i]==0 or arr[i]==1:
+for i in arr:
+    if(i==0 or i==1):
         c+=1
-if c==n:
-    print("True")
+if(c==n):
+    print(True)
 else:
-    print("False")
+    print(False)
