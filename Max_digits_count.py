fb4bbb97d7b66d9f3bf9c945223eff148b7bf90a
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-10-25 11:33:22 +0530

    y = int(input()) x = list(map(int,input().split())) n = len(str(max(x))) b=[] for i in x:     b.append(len(str(i))) print(b.count(n))

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,24 +1,7 @@
-n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-#print(arr)
-l=[]
-d=0
-for i in range(n):
-    p=arr[i]
-    if(p==0):
-        d+=1
-    if(p<0):
-        p=p*-1
-    while(p):
-        d+=1
-        p=p//10
-    l.append(d)
-    d=0
-l.sort()
-#print(l)
-q=0
-max=l[n-1]
-for i in range(n):
-    if(l[i]==max):
-        q+=1
-print(q)
+y = int(input())
+x = list(map(int,input().split()))
+n = len(str(max(x)))
+b=[]
+for i in x:
+    b.append(len(str(i)))
+print(b.count(n))
