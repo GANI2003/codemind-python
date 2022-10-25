3165ba92a72baa1b9bbdf4d86e1ddcc8b1ef8027
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-10-25 11:34:34 +0530

    a=str(input()) b=a.lower() c=list(b) d=[] f=[] e='abcdefghijklmnopqrstuvwxyz' for i in c:     if i!=' ':         if i not in d:             d.append(i) for i in e:     if i in d:         f.append(i) g=''.join(f) print(g)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,15 @@
-n=input()
-q=n.lower()
-l=[]
-for i in q:
-    if i.isalpha() and q.count(i)==1:
-        l.append(i)
-l.sort()
-for i in l:
-    print(i,end="")
+a=str(input())
+b=a.lower()
+c=list(b)
+d=[]
+f=[]
+e='abcdefghijklmnopqrstuvwxyz'
+for i in c:
+    if i!=' ':
+        if i not in d:
+            d.append(i)
+for i in e:
+    if i in d:
+        f.append(i)
+g=''.join(f)
+print(g)
