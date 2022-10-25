d02b83b04722acc4d696cbb3eeed6e944a4c18be
GANI2003 <104790586+GANI2003@users.noreply.github.com>
2022-10-25 11:31:27 +0530

    s=input().lower() s=s.split() s1='' l=list(s[0]) s.remove(s[0]) f=0 for i in l:     for j in s:         if i not in j:             f=1             break     else:         s1+=i
    if len(s1)==0:
        print(-1)
    else:
        print(s1)

--- Common_characters_-1.py
+++ Common_characters_-1.py
@@ -1,17 +1,18 @@
-n=input()
-n=n.lower()
-n=n.split()
-k=''
-l=str(n[0])
-for j in l:
-    c=0
-    for x in range(1,len(n)):
-        n[x]=str(n[x])
-        if j in n[x]:
-            c+=1
-    if c==len(n)-1:
-        k+=j
-if len(k)==0:
+s=input().lower()
+s=s.split()
+s1=''
+l=list(s[0])
+s.remove(s[0])
+f=0
+for i in l:
+    for j in s:
+        if i not in j:
+            f=1
+            break
+    else:
+        s1+=i
+        
+if len(s1)==0:
     print(-1)
 else:
-    print(k)
+    print(s1)
